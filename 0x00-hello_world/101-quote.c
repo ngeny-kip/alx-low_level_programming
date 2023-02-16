51e0f68153fd8f1ac09e10974a8d9038d5751041
ngeny-kip <gideonkorir78@gmail.com>
2023-02-17 02:37:14 +0300

    UNIX

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistdio.h>
+
+/**
+ * main	- a C programe that prints a line to unistdio header
+ * Return: print the success of a programe
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\"  Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+
