4e0cf5a158a4d6f69e2f75ef63c021070e218a8a
ngeny-kip <gideonkorir78@gmail.com>
2023-02-17 00:19:24 +0300

    hello, printf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main-entry point
+ * return:retutns a successful value
+ */
+
+int main(void)
+{
+	printf("Programming is like building a multilingual puzzle.\n");
+	return (0);
+}
