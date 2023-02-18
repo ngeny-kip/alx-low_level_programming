4c491845579b44ee68699c1ff6cd3a03f333fb5b
ngeny-kip <gideonkorir78@gmail.com>
2023-02-18 22:14:54 +0300

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *  main - Entry point
+ *  Return: zero (success)
+ */
+int main(void)
+{
+	char n;
+
+	n = 'a';
+	while
+		(n <= 'z') {
+			putchar(n);
+			n++;
+		}
+	putchar('\n');
+	return (0);
+}
