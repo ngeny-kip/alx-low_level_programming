dddc897e0acc3672f9b1f6a518645b0a8949a5d7
ngeny-kip <gideonkorir78@gmail.com>
2023-02-19 12:24:09 +0300

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *  main - Entry point
+ *  Return: zero (success)
+ */
+int main(void)
+{
+	char n;
+
+	n = '0';
+	while
+		(n <= '9') {
+			putchar(n);
+			n++;
+		}
+	putchar('\n');
+	return (0);
+}
