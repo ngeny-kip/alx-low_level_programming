f67d6fe5ab904e72d22845f8ee23728ab68630d7
ngeny-kip <gideonkorir78@gmail.com>
2023-02-21 22:05:35 +0300

    0_putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -prints putchar
+ * Return: returns zero(successful)
+ */
+
+int main(void)
+{
+	int g;
+
+	g = 5;
+	if (g <= 5)
+		printf("0_putchar\n");
+	return (0);
+}
