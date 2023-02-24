b14fbcb074fd2f5dd9587bc954be860c5033690f
ngeny-kip <gideonkorir78@gmail.com>
2023-02-24 23:24:20 +0300

    0 > 972

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -19,10 +19,14 @@ int largest_number(int a, int b, int c)
     {
         largest = b;
     }
-    else if 
+    else if (c > b)
     {
         largest = c;
     }
+	else
+	{
+		largest = b;
+	}
 
     return (largest);
 }
