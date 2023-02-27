ad30bc510fd270f5fe5f07b565a8e341ae137e34
ngeny-kip <gideonkorir78@gmail.com>
2023-02-28 00:16:31 +0300

    prints in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev -prints a string in reverse
+ * @s: string to be printed
+ * Return: Return 0 successful
+ */
+void print_rev(char *s)
+{
+	int revs = 0;
+	int o;
+
+	while (*s != \'0')
+	{
+		revs++;
+		s++;
+	}
+	s--;
+	for (o = revs; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
