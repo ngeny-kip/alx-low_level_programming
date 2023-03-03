76ecc932b9623d228b9bde3e61b9bf5b794bc28a
ngeny-kip <gideonkorir78@gmail.com>
2023-03-03 05:59:56 +0300

    a

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * leet - encode a string into 1337
+ * @n: input value
+ * Return: n value
+ */
+char *leet(char *n)
+{
+	int x, y;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (x = 0; n[x] != '\0'; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			if (n[x] == s1[y])
+			{
+				n[x] = s2[y];
+			}
+		}
+	}
+	return (n);
+}
+
