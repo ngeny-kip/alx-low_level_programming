c2fb71aa8e08dcd40d6f4efe92d376a7c8b69e79
ngeny-kip <gideonkorir78@gmail.com>
2023-03-07 12:07:10 +0300

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strstr - Entry point
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0 (Success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *x = haystack;
+		char *y = needle;
+
+		while (*x == *y && *y != '\0')
+		{
+			x++;
+			y++;
+		}
+
+		if (*y == '\0')
+			return (haystack);
+	}
+
+	return (0);
+}
+
