9eeea598db1156c58b887b29261259058433d255
ngeny-kip <gideonkorir78@gmail.com>
2023-03-13 21:54:17 +0300

    multiplies two numbers

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the name of the programme
+ * @argc: Count arguments
+ * @argv: Array argument
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int count = 0;
+
+	if (argc > 0)
+	{
+		while
+			(count < argc)
+	}
+	printf("%s\n", argv[count]);
+
+	return (0);
+}
