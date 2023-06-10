108f120c6ca46718ebd4d10ab27aaef7c56587b6
ngeny-kip <gideonkorir78@gmail.com>
2023-06-10 23:28:13 +0300

    prints all arguments

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,9 +9,9 @@
  *
  * Return: Always 0
  */
-int main(int argc, char *argv[])
+int main(int argc_attribute_((unused)), char *argv[])
 {
-	printf("%s\n", argv[0]);
+	printf("%s\n", *argv);
 
 	return (0);
 }
