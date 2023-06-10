d4e00d2a848383e1d9c6acde3da0fafc67ac489a
ngeny-kip <gideonkorir78@gmail.com>
2023-06-10 23:02:04 +0300

    prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main- prints the name of the programme
+ * @argc: Counts arguments
+ *
+ * @argv: array of arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char* argv[])
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
