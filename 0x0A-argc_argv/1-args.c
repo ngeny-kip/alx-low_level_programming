2651f5c4a642569f25e1bfde5b018b3d660acd9c
ngeny-kip <gideonkorir78@gmail.com>
2023-06-10 23:22:18 +0300

    prints all arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the name of the programme
+ * @argc: counts arguments
+ * @argv: Array of arguments
+ *
+ * Return: Always (0)
+ */
+int main(int argc, char *argv[])
+{
+    printf("%i\n ", argc - 1);
+
+    return (0);
+
+}
