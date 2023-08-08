1a60d96baef34a35e3e8583cc0066e7233a4e4e4
ngeny-kip <gideonkorir78@gmail.com>
2023-08-08 08:27:59 +0300

    library

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <stdio.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
